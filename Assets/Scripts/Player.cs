f4c7bc90da2556cf92a812829c80b0df81df5dc9
git-jslee <datadam@kakao.com>
2022-05-26 20:58:01 +0900

    플레이어 이동

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,12 +11,13 @@ public class Player : MonoBehaviour
 
     Vector3 moveVec;
 
-    Animator anim;
+    public Animator anim;
 
     // Start is called before the first frame update
     void Awaik()
     {
-        anim = GetComponentInChildren<Animator>();
+        // anim = GetComponentInChildren<Animator>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,13 +25,16 @@ public class Player : MonoBehaviour
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
-        wDown = Input.GetButtonDown("Walk");
+        wDown = Input.GetButton("Walk");
 
         moveVec = new Vector3(hAxis, 0f, vAxis).normalized;
 
-        transform.position += moveVec * speed * Time.deltaTime;
-        Debug.Log("**" + anim);
-        // anim.SetBool("isRun", moveVec != Vector3.zero);
-        // anim.SetBool("isWalk", wDown);
+        transform.position += moveVec * speed * (wDown ? 0.3f : 1f) *Time.deltaTime;
+        // Debug.Log("**" + anim);
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+        anim.SetBool("isWalk", wDown);
+
+        // 케릭터가 나아가는 방향으로 바라보기 구현
+        transform.LookAt(transform.position + moveVec);
     }
 }
